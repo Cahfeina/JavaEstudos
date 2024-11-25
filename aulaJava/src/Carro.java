3696516b02a4d5565e24e83b8be37bd425c956c6
camilaborgesoliveira <camilab.oliveira@outlook.com>
2024-11-25 19:35:07 -0300

    metodos com retorno

--- aulaJava/src/Carro.java
+++ aulaJava/src/Carro.java
@@ -7,8 +7,27 @@ public class Carro {
     double precoCompra;
     Pessoa proprietario;
 
+    void imprimirResumoDepeciacao(){
+        double valorRevendaMeuCarro = calcularValorRevenda();
+        int tempoDeUsoMeuCarro = calcularTempoDeUsoEmAnos();
+        double valorIpva = calcularIpva();
+
+        System.out.printf("Tempo de uso (anos): %d%n", tempoDeUsoMeuCarro);
+        System.out.printf("Valor de revenda: %6.2f%n", valorRevendaMeuCarro);
+        System.out.printf("Valor Ipva: %6.2f%n", valorIpva);
+    }
+
+    double calcularIpva(){
+        return calcularValorRevenda() * 0.04;
+    }
+
+    int calcularTempoDeUsoEmAnos() {
+        return 2022 - anoFabricacao;
+    }
+
+
     double calcularValorRevenda() {
-        int tempoDeUsoEmAnos = 2022 - anoFabricacao;
+        int tempoDeUsoEmAnos = calcularTempoDeUsoEmAnos();
         int vidaUtilEmAnos = 20;
         double valorRevenda = (precoCompra / vidaUtilEmAnos) * (vidaUtilEmAnos - tempoDeUsoEmAnos);
         if (valorRevenda < 0) {
